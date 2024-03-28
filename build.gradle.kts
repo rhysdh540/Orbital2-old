9d2073eadc7fbd0ab99d8efb9901e8320d1d5249
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-03-28 08:50:55 -0400

    shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     idea
     id("edu.sc.seis.macAppBundle") version "2.3.1"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "maven_group"()
@@ -22,28 +23,26 @@ macAppBundle {
     javaExtras["-XstartOnFirstThread"] = null
 }
 
-val include: Configuration by configurations.creating
-
 dependencies {
-    // runtime dependency on the old version so we can see it but cant reference it
-    runtimeOnly(files("libs/orbital-old.jar"))
+    // test runtime dependency on the old version so we can see it but cant reference it
+    testRuntimeOnly(files("libs/orbital-old.jar"))
 
-    include(implementation(("org.joml:joml:${"joml_version"()}"))!!)
+    shadow(implementation(("org.joml:joml:${"joml_version"()}"))!!)
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
 
     val lwjglVersion = "lwjgl_version"()
     val nativesPlatforms = "lwjgl_natives"().split(", ")
     val modules = "lwjgl_modules"().split(", ")
 
-    include(implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))!!)
-    include(implementation("org.lwjgl:lwjgl")!!)
+    shadow(implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))!!)
+    shadow(implementation("org.lwjgl:lwjgl")!!)
     for(platform in nativesPlatforms) {
-        include(runtimeOnly("org.lwjgl:lwjgl:$lwjglVersion:natives-$platform")!!)
+        shadow(runtimeOnly("org.lwjgl:lwjgl:$lwjglVersion:natives-$platform")!!)
     }
     for(module in modules) {
-        include(implementation("org.lwjgl:lwjgl-$module")!!)
+        shadow(implementation("org.lwjgl:lwjgl-$module")!!)
         for(platform in nativesPlatforms) {
-            include(runtimeOnly("org.lwjgl:lwjgl-$module:$lwjglVersion:natives-$platform")!!)
+            shadow(runtimeOnly("org.lwjgl:lwjgl-$module:$lwjglVersion:natives-$platform")!!)
         }
     }
 }
@@ -51,7 +50,7 @@ dependencies {
 tasks.register<JavaExec>("run") {
     dependsOn(tasks.jar)
     mainClass = "samuschair.orbital2.Orbital2Main"
-    classpath(configurations.runtimeClasspath.get(), include, tasks.jar.get())
+    classpath(configurations.runtimeClasspath.get(), tasks.jar.get())
     jvmArgs = listOf("-Dorg.lwjgl.util.Debug=true", "-XstartOnFirstThread")
 }
 
@@ -59,17 +58,19 @@ tasks.jar {
     manifest {
         attributes["Main-Class"] = "samuschair.orbital2.Orbital2Main"
     }
+}
 
-    from(include.map {
-        if (it.isDirectory)
-            it
-        else
-            zipTree(it).matching {
-                exclude("META-INF/**")
-            }
-    })
+tasks.shadowJar {
+    from(tasks.jar.get())
+    exclude {
+        it.path.startsWith("META-INF/") && it.path != "META-INF/MANIFEST.MF"
+    }
 
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    archiveClassifier = "all"
+}
+
+tasks.assemble {
+    dependsOn(tasks.shadowJar.get())
 }
 
 operator fun String.invoke(): String = rootProject.ext[this] as? String
